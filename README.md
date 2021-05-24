7f63f3c6c5da1f2e1350ca5f97ce0f97eea5d41e
marton <marton@near.st>
2021-05-24 21:51:45 +0100

    slack instad of telegram

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 Get notified when you become eligible for a vaccine in the UK.
 
-This is a simple Lambda function that fetches and parses the NHS website once an hour (scheduled by CloudWatch Events) and sends you a Telegram message if you become eligible (age can be configured in the `.env` file).
+This is a simple Lambda function that fetches and parses the NHS website once an hour (scheduled by CloudWatch Events) and sends you a Slack message if you become eligible.
 
 ## Deploy
 
@@ -12,8 +12,4 @@ yarn build
 yarn cdk deploy
 ```
 
-## Configure
-
 Rename the `.env.example` to `.env` and fill the parameters.
-
-You need to create a Telegram Bot and set your Telegram token and chat id. More info about the Telegram bot API [here](https://github.com/yagop/node-telegram-bot-api).
--- functions/index.js
+++ functions/index.js
@@ -1,16 +1,19 @@
 const axios = require("axios");
-const TelegramBot = require("node-telegram-bot-api");
+const aws = require("aws-sdk");
+const dynamo = new aws.DynamoDB.DocumentClient();
 
-const myAge = process.env.MY_AGE;
-const token = process.env.TELEGRAM_TOKEN;
-const chatId = process.env.CHAT_ID;
 const nhsURL =
   "https://www.nhs.uk/conditions/coronavirus-covid-19/coronavirus-vaccination/book-coronavirus-vaccination/";
 
-const bot = new TelegramBot(token, { polling: true });
-
-async function sendMessage(message, options = {}) {
-  await bot.sendMessage(chatId, message, options);
+async function sendMessage(message) {
+  await axios.post(
+    "https://slack.com/api/chat.postMessage",
+    {
+      channel: process.env.SLACK_CHANNEL,
+      text: message,
+    },
+    { headers: { Authorization: `Bearer ${process.env.SLACK_TOKEN}` } }
+  );
 }
 
 async function main() {
@@ -21,16 +24,25 @@ async function main() {
     /you&#x27;ll turn (\d*) [a-zA-Z0-9 ]+/
   );
 
-  const message = `💉  Appointment Available! Book now if 1) ${line.replace(
-    "&#x27;",
-    "'"
-  )} or 2) ${line2.replace("&#x27;", "'")}. ${nhsURL}`;
+  const ages = await dynamo
+    .scan({ TableName: process.env.TABLE_NAME })
+    .promise();
 
-  if (+age <= myAge) {
-    console.log(message);
-    await sendMessage(message);
-  } else if (+ageTurn <= myAge) {
-    console.log(message);
+  const smallest = ages.Items.sort((a, b) => a.age - b.age)[0];
+
+  if (+smallest.age !== +age) {
+    await dynamo
+      .put({
+        TableName: process.env.TABLE_NAME,
+        Item: { age: +age, timestamp: new Date().toISOString() },
+      })
+      .promise();
+
+    const message = `:syringe: Appointment Available! Book now if 1) ${line.replace(
+      "&#x27;",
+      "'"
+    )} or 2) ${line2.replace("&#x27;", "'")}. ${nhsURL}`;
+    console.log({ message });
     await sendMessage(message);
   } else {
     console.log("Not yet... ⏳", { line, line2 });
